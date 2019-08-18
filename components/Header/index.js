109b9857678ecdf852f44386c3c4139ee59d6a86
Warren Rawlings <rawlings.warren@gmail.com>
2019-08-18 19:48:55 -0400

    header is in, it looks odd

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,23 +9,27 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
-const head = document.createElement('div');
-head.classList.add('header');
+function Header() {
+    const head = document.createElement('div');
+    head.classList.add('header');
 
-const span = document.createElement('span');
-span.classList.add('date');
-span.textContent = 'MARCH 28, 2019';
+    const span = document.createElement('span');
+    span.classList.add('date');
+    span.textContent = 'MARCH 28, 2019';
 
-const h1 = document.createElement('h1');
-span.textContent = 'Lambda Times';
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Lambda Times';
 
-const span2 = document.createElement('span');
-span.classList.add('temp');
-span.textContent = '98°';
+    const span2 = document.createElement('span');
+    span2.classList.add('temp');
+    span2.textContent = '98°';
 
-const headControl = document.querySelector('.header-container');
-headControl.appendChild(head);
-headControl.appendChild(span);
-headControl.appendChild(h1);
-headControl.appendChild(span2);
+    const headControl = document.querySelector('.header-container');
+    headControl.appendChild(head);
+    headControl.appendChild(span);
+    headControl.appendChild(h1);
+    headControl.appendChild(span2);
+
+}
+//console.log(Header);
+Header();
